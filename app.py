a01a56b418219a2a16df4bab6a9dbeaceab6c7e3
Rohan Singh <rohan146810@gmail.com>
2024-07-05 19:11:01 -0400

    added certifi

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ from flask import Flask
 from routes import pages
 from dotenv import load_dotenv
 
+import certifi
+ca=certifi.where()
+
 load_dotenv
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(pages)
-    client = MongoClient(os.environ.get("MONGODB_URI"))
+    client = MongoClient(os.environ.get("MONGODB_URI"), tlsCAFile=ca)
     app.db = client.get_default_database()
 
     return app
